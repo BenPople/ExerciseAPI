45c3baa0c2dcd57fc1515e5433444ebb2fdf435f
Ben Pople <benpople@outlook.com>
2023-03-06 14:55:18 +0000

    Changed schema and procedurally generated data.

--- initflaskdb.py
+++ initflaskdb.py
@@ -1,5 +1,7 @@
 import sqlite3
 import hashlib
+import random
+import datetime
 
 HASH_SALT = "L6SmartAppDevelopment"
 
@@ -11,11 +13,62 @@ def initdb():
 
     cur = connection.cursor()
 
+    #Insert example users
     cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
                 ('BenPople', hashlib.md5(('H4rdw4re' + HASH_SALT).encode()).hexdigest(), 'popleb@btc.ac.uk'))
     
     cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
-                ('AbigailCollard', hashlib.md5(('H4rdw4re' + HASH_SALT).encode()).hexdigest(), 'collarda@btc.ac.uk'))
+                ('SimonWest', hashlib.md5(('H4rdw4re' + HASH_SALT).encode()).hexdigest(), 'wests@btc.ac.uk'))
+
+    cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+            ('EmilyWang', hashlib.md5(('p@ssword1' + HASH_SALT).encode()).hexdigest(), 'emilyw@xyz.com'))
+
+    cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                ('MikeSmith', hashlib.md5(('myPa$$w0rd' + HASH_SALT).encode()).hexdigest(), 'mikes@abc.com'))
+
+    cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                ('AliceJones', hashlib.md5(('secret12' + HASH_SALT).encode()).hexdigest(), 'alicej@def.com'))
+
+    cur.execute("INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+                ('BobLee', hashlib.md5(('qwerty123' + HASH_SALT).encode()).hexdigest(), 'bobl@ghi.com'))
+
+    #Insert example excersise types
+    cur.execute("INSERT INTO exercisetypes (name, description) VALUES (?, ?)",
+            ('Running', 'A cardiovascular exercise that involves running or jogging.'))
+
+    cur.execute("INSERT INTO exercisetypes (name, description) VALUES (?, ?)",
+                ('Weightlifting', 'An exercise that involves lifting weights to build strength and muscle mass.'))
+
+    cur.execute("INSERT INTO exercisetypes (name, description) VALUES (?, ?)",
+                ('Swimming', 'A low-impact exercise that involves swimming laps in a pool or other body of water.'))
+
+    cur.execute("INSERT INTO exercisetypes (name, description) VALUES (?, ?)",
+                ('Yoga', 'A series of postures and breathing exercises designed to promote relaxation and flexibility.'))
+
+    cur.execute("INSERT INTO exercisetypes (name, description) VALUES (?, ?)",
+                ('Cycling', 'A cardiovascular exercise that involves cycling on a stationary or moving bike.'))
+
+    #Insert example excersise activities
+    #Define a list of user IDs and exercise type IDs
+    user_ids = [1, 2, 3, 4, 5]
+    exercise_type_ids = [1, 2, 3, 4, 5]
+
+    #Generate 100 random exercise entries
+    for i in range(100):
+        #Select a random user ID and exercise type ID
+        user_id = random.choice(user_ids)
+        exercise_type_id = random.choice(exercise_type_ids)
+        
+        #Generate a random duration between 10 and 60 minutes
+        duration_minutes = random.randint(10, 60)
+        
+        #Generate a random date within the last 30 days
+        days_ago = random.randint(0, 30)
+        date_completed = datetime.date.today() - datetime.timedelta(days=days_ago)
+        
+        #Insert the new exercise entry into the exercises table
+        cur.execute("INSERT INTO exercises (user_id, exercisetype_id, duration_minutes, date_completed) VALUES (?, ?, ?, ?)",
+                    (user_id, exercise_type_id, duration_minutes, date_completed))
 
     connection.commit()
     connection.close()
--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS exercisetypes;
+DROP TABLE IF EXISTS exercises;
 
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,4 +13,14 @@ CREATE TABLE exercisetypes (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     description TEXT NOT NULL
+);
+
+CREATE TABLE exercises (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    exercisetype_id INTEGER NOT NULL,
+    duration_minutes INTEGER NOT NULL,
+    date_completed DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (exercisetype_id) REFERENCES exercisetypes(id)
 );
